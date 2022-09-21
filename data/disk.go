0d27338fb9e2b38912bafa68d46f9e18fcc0eb4d
LemuriaX <xialei1468@gmail.com>
2022-09-21 23:40:30 +0800

    feat: 硬盘容量获取

--- data/disk.go
+++ data/disk.go
@@ -1,36 +1,66 @@
 package data
 
 import (
+	"context"
+	"fmt"
 	"github.com/gogf/gf/v2/encoding/gjson"
+	"github.com/gogf/gf/v2/os/gcache"
 	"github.com/gogf/gf/v2/os/gctx"
 	"github.com/gogf/gf/v2/os/gproc"
+	"github.com/gogf/gf/v2/text/gstr"
+	"strconv"
 )
 
 type DiskInfo struct {
-	Name          string
-	Temperature   float32
-	Size          float32
-	Usage         float32
-	Free          float32
-	OccupancyRate float32
-	Read          float32
-	Write         float32
+	Name        string
+	Temperature int
+	Size        string
+	// Usage         float32
+	// Free          float32
+	// OccupancyRate float32
+	Read  float32
+	Write float32
 }
 
-func GetAllDisk() []string {
-	resultString, _ := gproc.ShellExec(gctx.New(), "lsblk --j -d")
+func GetAllDisk() map[string]float64 {
+	resultString, _ := gproc.ShellExec(gctx.New(), "lsblk --j -db")
 	resultJsons := gjson.New(resultString).Get("blockdevices").Array()
-	var diskList []string
+	diskMap := map[string]float64{}
 	for _, resultJson := range resultJsons {
 		result := resultJson.(map[string]interface{})
-		diskList = append(diskList, "/dev/"+result["name"].(string))
+		diskMap["/dev/"+result["name"].(string)] = result["size"].(float64)
 	}
-	return diskList
+	return diskMap
 }
 
-func GetDiskInfo(diskName string) *DiskInfo {
+func GetDiskTemperature(diskName string) int {
+	result := 0
+	if gstr.Contains(diskName, "nvme") {
+		resultString, _ := gproc.ShellExec(gctx.New(), "nvme smart-log "+diskName+" | grep '^temperature' ")
+		result, _ = strconv.Atoi(gstr.Trim(gstr.StrTillEx(gstr.StrEx(resultString, ":"), "C")))
+
+	} else {
+		resultString, _ := gproc.ShellExec(gctx.New(), "hddtemp -n "+diskName)
+		resultString = gstr.Trim(resultString, "\n")
+		if gstr.IsNumeric(resultString) {
+			result, _ = strconv.Atoi(resultString)
+		}
+	}
+
+	return result
+}
+
+func GetDiskInfo(ctx context.Context, diskName string) *DiskInfo {
 	diskInfo := &DiskInfo{}
 	diskInfo.Name = diskName
+	diskInfo.Temperature = GetDiskTemperature(diskName)
+	diskMap, _ := gcache.Get(ctx, "diskMap")
+	size := diskMap.MapStrAny()[diskName].(float64) / (1024 * 1024)
+	if size > 1024 {
+		diskInfo.Size = fmt.Sprintf("%.2f GB", size/1024)
+	} else {
+		diskInfo.Size = fmt.Sprintf("%.2f MB", size)
+	}
 
 	return diskInfo
 }
--- main.go
+++ main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"fmt"
 	_ "github.com/gogf/gf/contrib/drivers/sqlite/v2"
-	"server_monitor/data"
+	"github.com/gogf/gf/v2/os/gcache"
+	"github.com/gogf/gf/v2/os/gctx"
+	data "server_monitor/data"
 	"server_monitor/util"
+	"time"
 )
 
 func main() {
+	ctx := gctx.New()
 	_ = util.InitDB()
 	cpuInfo := data.GetCPUInfo()
 	fmt.Printf("%v\n", cpuInfo)
-	diskInfo := data.GetDiskInfo()
-	fmt.Printf("%v\n", diskInfo)
+	diskMap := data.GetAllDisk()
+	gcache.Set(ctx, "diskMap", diskMap, time.Minute)
+	for key, _ := range diskMap {
+		diskInfo := data.GetDiskInfo(ctx, key)
+		fmt.Printf("%v\n", diskInfo)
+	}
+
 }
